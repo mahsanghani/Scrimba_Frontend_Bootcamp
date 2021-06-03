102b8df0ca39e4b63b4c3f9d740a8f85a29900c8
Ahsan Ghani <ghaniahsan@outlook.com>
2021-06-03 13:24:03 -0400

    finished counter application

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
   count: number = 5;
+  countStr: string = "";
   countEl: any;
+  saveEl: any;
   name: string = "Per Harald Borgen";
   greeting: string = "Hi, my name is ";
   myGreeting: string = this.greeting + this.name;
@@ -17,6 +19,7 @@ export class CounterComponent implements OnInit {
 
   ngOnInit(): void {
     this.countEl = document.getElementById("count-el");
+    this.saveEl = document.getElementById("save-el");
   }
 
   increment() {
@@ -28,6 +31,9 @@ export class CounterComponent implements OnInit {
   }
 
   save() {
-    console.log(this.count)
+    this.countStr = this.count + " - "
+    this.saveEl.textContenttStr
+    this.countEl.textContent = 0
+    this.count = 0
   }
 }
