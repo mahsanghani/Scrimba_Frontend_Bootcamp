907a54b5c3062d15dc69c0f32cb731dc22f8be79
Ahsan Ghani <ghaniahsan@outlook.com>
2021-06-04 15:58:12 -0400

    started blackjack

--- src/app/blackjack/blackjack.component.ts
+++ src/app/blackjack/blackjack.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blackjack.component.css']
 })
 export class BlackjackComponent implements OnInit {
+  firstCard: number = 10;
+  secondCard: number = 11;
+  sum: number = 0;
+  hasBlackJack: boolean = false;
+  isAlive: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.firstCard = 10
+    this.secondCard = 11
+    this.sum = this.firstCard + this.secondCard + 4
+    this.hasBlackJack = false
+    this.isAlive = true
 
+    if (this.sum <= 20) {
+      console.log("Do you want to draw a new card? 🙂")
+    } else if (this.sum === 21) {
+      console.log("Wohoo! You've got Blackjack! 🥳")
+      this.hasBlackJack = true
+    } else {
+      console.log("You're out of the game! 😭")
+      this.isAlive = false
+    }
+  }
 }
--- src/app/challenges/challenges.component.ts
+++ src/app/challenges/challenges.component.ts
@@ -12,4 +12,9 @@ export class ChallengesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Create two variables, firstName and lastName
+
+  // Concatenate the two variables into a third variable called fullName
+
+  // Log fullName to the console
 }
