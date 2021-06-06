49907c3f8467095435321531305326cd9fe0ba7c
Ahsan Ghani <ghaniahsan@outlook.com>
2021-06-06 18:07:00 -0400

    successfully completed second set of challenges

--- src/app/challenges2/challenges2.component.ts
+++ src/app/challenges2/challenges2.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./challenges2.component.css']
 })
 export class Challenges2Component implements OnInit {
+  appleShelf: any;
+  orangeShelf: any;
 
-  constructor() { }
+  constructor() {
+    this.appleShelf = document.getElementById("apple-shelf")
+    this.orangeShelf = document.getElementById("orange-shelf")
+  }
 
   ngOnInit(): void {
     let person = {
@@ -19,6 +24,63 @@ export class Challenges2Component implements OnInit {
       console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
     }
     logData()
-  }
 
+    let age = 75
+
+    if (age < 6) {
+      console.log("free")
+    } else if (age < 18) {
+      console.log("child discount")
+    } else if (age < 27) {
+      console.log("student discount")
+    } else if (age < 67) {
+      console.log("full price")
+    } else {
+      console.log("senior citizen discount")
+    }
+
+    let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+    console.log("The 5 largest countries in the world:")
+    for (let i = 0; i < largeCountries.length; i++) {
+      console.log("- " + largeCountries[i])
+    }
+
+    let largeCountries2 = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+    largeCountries2.pop()
+    largeCountries2.push("Pakistan")
+    largeCountries2.shift()
+    largeCountries2.unshift("China")
+    console.log(largeCountries2)
+
+    let dayOfMonth = 13
+    let weekday = "Friday"
+    if (dayOfMonth === 13 && weekday === "Friday") {
+      console.log("😱")
+    }
+
+    let hands = ["rock", "paper", "scissor"]
+    function getHand() {
+      let randomIndex = Math.floor( Math.random() * 3)
+      return hands[randomIndex]
+    }
+    console.log(getHand())
+
+    let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+    this.appleShelf = document.getElementById("apple-shelf")
+    this.orangeShelf = document.getElementById("orange-shelf")
+
+    const sortFruit = () => {
+      for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+          this.appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+          this.orangeShelf.textContent += "🍊"
+        }
+      }
+    }
+
+    sortFruit()
+  }
 }
