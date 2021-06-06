32e1973c62f8e7e48c4f6f7d257084f1bc90d7a0
mahsanghani <ghaniahsan@outlook.com>
2021-06-06 11:31:18 -0400

    successfully completed blackjack

--- src/app/blackjack/blackjack.component.ts
+++ src/app/blackjack/blackjack.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {formatNumber} from "@angular/common";
 
 @Component({
   selector: 'app-blackjack',
@@ -9,6 +8,8 @@ import {formatNumber} from "@angular/common";
 export class BlackjackComponent implements OnInit {
   firstCard: number = 10;
   secondCard: number = 9;
+  min: number = 1;
+  max: number = 11;
   sum: number = 0;
   hasBlackJack: boolean = false;
   isAlive: boolean = false;
@@ -16,48 +17,115 @@ export class BlackjackComponent implements OnInit {
   messageEl: any;
   sumEl: any;
   cardsEl: any;
+  greetingEl: any;
+  playerEl: any;
   nextCard: number = 2;
   cards: number[] = [];
+  Player = {
+    name: "Ahsan",
+    chips: 1234,
+    sayHello: function() {
+      console.log("Heisann!")
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.firstCard = 10
-    this.secondCard = 11
-    this.nextCard = 2
-    this.cards = [this.firstCard,this.secondCard,this.nextCard]
-    this.sum = this.firstCard + this.secondCard
-    this.hasBlackJack = false
-    this.isAlive = true
-    this.messageEl = document.getElementById("message-el")
-    this.sumEl = document.getElementById("sum-el")
-    this.cardsEl = document.getElementById("cards-el")
+    this.firstCard = this.getRandomCard(1,14);
+    this.secondCard = this.getRandomCard(1,14);
+    this.nextCard = this.getRandomCard(1,14);
+    this.min = 1;
+    this.max = 11;
+    this.cards = [this.firstCard,this.secondCard];
+    this.sum = this.firstCard + this.secondCard;
+    this.hasBlackJack = false;
+    this.isAlive = true;
+    this.messageEl = document.getElementById("message-el");
+    this.sumEl = document.getElementById("sum-el");
+    this.cardsEl = document.getElementById("cards-el");
+    this.greetingEl = document.getElementById("greeting-el");
+    this.playerEl = document.getElementById("player-el")
+    this.Player = {
+      name: "Ahsan",
+      chips: 1234,
+      sayHello: function() {
+        console.log("Heisann!")
+      }
+    }
   }
 
   startGame(): void {
-    this.renderGame()
+    this.isAlive = true;
+    this.firstCard = this.getRandomCard(1,14);
+    this.secondCard = this.getRandomCard(1,14);
+    this.cards = [this.firstCard, this.secondCard];
+    this.sum = this.firstCard + this.secondCard;
+    this.renderGame();
   }
 
   renderGame(): void {
-    this.cardsEl.textContent = "Cards: " + this.cards[0] + " " + this.cards[1]
-    this.sumEl.textContent = "Sum: " + this.sum
+    this.cardsEl.textContent = "Cards: ";
+    for (let i=0;i<this.cards.length;i++){
+      this.cardsEl.textContent += this.cards[i] + " ";
+    }
+    this.sumEl.textContent = "Sum: " + this.sum;
     if (this.sum <= 20) {
       this.message = "Do you want to draw a new card?";
     } else if (this.sum === 21) {
       this.message = "Woohoo! You've got Blackjack!";
-      this.hasBlackJack = true
+      this.hasBlackJack = true;
     } else {
       this.message = "You're out of the game!";
-      this.isAlive = false
+      this.isAlive = false;
     }
     console.log(this.message);
     this.messageEl.textContent = this.message;
+    this.playerEl.textContent = this.Player.name + ": $" + this.Player.chips
   }
 
   newCard(): void {
-    console.log("Drawing a new card from the deck!");
-    this.nextCard = 11;
-    this.sum += this.nextCard
-    this.renderGame()
+    /*if(this.isAlive===true&&this.hasBlackJack===false)*/
+    if(this.isAlive&&!this.hasBlackJack){
+      console.log("Drawing a new card from the deck!");
+      this.nextCard = this.getRandomCard(1,14);
+      this.sum += this.nextCard;
+      this.cards.push(this.nextCard);
+      this.renderGame();
     }
+    for (let i=0;i<this.cards.length;i++){
+      console.log(this.cards[i]);
+    }
+    for (let i=1;i<11;i++){
+      console.log(i);
+    }
+    let messages = [
+      "Hey, how's it going?",
+      "I'm great, thank you! How about you?",
+      "All good. Been working on my portfolio lately.",
+      "Same here!",
+      "Great to hear",
+      "🙌"
+    ]
+    for (let i=0;i<messages.length;i++) {
+      console.log(messages[i]);
+    }
+    let sentence = ["Hello ", "my ", "name ", "is ", "Per"];
+    for (let i = 0; i < sentence.length; i++) {
+      this.greetingEl.textContent += sentence[i] + " ";
+    }
+  }
+
+  getRandomCard(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let random_number = Math.floor(Math.random() * (max - min)) + min;
+    if (random_number === 1) {
+      return 11;
+    } else if (random_number > 9) {
+      return 10;
+    } else {
+      return random_number
+    }
+  }
 }
