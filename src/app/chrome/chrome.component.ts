b8176a8da7071057fb60001a2e1762c05aa4bb6e
mahsanghani <ghaniahsan@outlook.com>
2021-06-07 20:11:05 -0400

    progressing chrome extension application

--- src/app/chrome/chrome.component.ts
+++ src/app/chrome/chrome.component.ts
@@ -12,11 +12,13 @@ export class ChromeComponent implements OnInit {
   inputEl: any;
   myLeads: string[] = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
   ulEl: any;
+  listItems: string = "";
 
   constructor() {
     this.box = " "
     this.inputBtn = " "
     this.inputEl = " "
+    this.listItems = ""
     this.box = document.getElementById("box")
     this.container = document.getElementById("container")
     this.inputBtn = document.getElementById("input-btn")
@@ -27,19 +29,15 @@ export class ChromeComponent implements OnInit {
   ngOnInit(): void {
     this.inputBtn.addEventListener("click", () => {
       console.log("Button clicked from addEventListener")
-      this.myLeads.push("www.awesomelead.com")
+      this.myLeads.push(this.inputEl.value)
+      this.inputEl.value = ""
       console.log(this.myLeads)
     })
     this.box.addEventListener("click", function(){
       console.log("I want to open the box!")
     })
-    for (let i = 0; i < this.myLeads.length; i++) {
-      this.ulEl.innerHTML = "<li>" + this.myLeads[i] + "</li>"
-      const li = document.createElement("li")
-      li.textContent = this.myLeads[i]
-      this.ulEl.append(li)
-    }
-    this.container.innerHTML = "<button>Buy!</button>"
+
+    this.renderLeads()
 
     const basePrice = 520
     const discount = 120
@@ -50,7 +48,26 @@ export class ChromeComponent implements OnInit {
     const fullPrice = basePrice - discount + shippingCost
     console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
   }
+
   buy() {
     this.container.innerHTML += "<p>Thank you for buying!</p>"
+  }
+
+  renderLeads() {
+    for (let i = 0; i < this.myLeads.length; i++) {
+      this.ulEl.innerHTML = "<li><a target='_blank' href='" + this.myLeads[i] + "'>" + this.myLeads[i] + "</a></li>"
+      this.listItems += `
+            <li>
+                <a target='_blank' href='${this.myLeads[i]}'>
+                    ${this.myLeads[i]}
+                </a>
+            </li>
+        `
+      const li = document.createElement("li")
+      li.textContent = this.myLeads[i]
+      this.ulEl.append(li)
+    }
+    this.ulEl.innerHTML = this.listItems
+    this.container.innerHTML = "<button>Buy!</button>"
   }
 }
